7c2391aab33ba31beed8c112681bbdab39f8d967
ryanoh42 <ryanoh42@gmail.com>
2021-12-31 00:31:56 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,28 +1,28 @@
 // REEL POP UP
-const btn = document.querySelector('.reel-btn'); //set constant for the "Watch Reel" button
-const videoContainer = document.querySelector('.video-container'); //set constant for the video-container
+const btn = document.querySelector('.watch-reel-btn'); //set constant for the "Watch Reel" button
+const reelContainer = document.querySelector('.reel-container'); //set constant for the video-container
 const close = document.querySelector('.close-video'); //set constant for the "x" button
 const vid = document.getElementById("myreel"); //set constant for the video/reel itself
 
-
-
-
 btn.addEventListener('click', () => {   //listens for a click on the "Watch reel" button
-    videoContainer.classList.add('show');   //adds the class .show to the video-container
+    reelContainer.classList.add('show');   //adds the class .show to the video-container
     close.classList.add('show');
 })
 close.addEventListener('click', () => { //listens for a click on the "x" button
-    videoContainer.classList.remove('show');    //removes the class .show from the video-container
+    reelContainer.classList.remove('show');    //removes the class .show from the video-container
     close.classList.remove('show');
 })
-// function playVid() {    //function to play the reel when it pops up
-//     setTimeout(function(){ vid.play(); }, 300); //delay playing the video for .3s
-// }
-// function pauseVid() {   //function to pause the reel when it closes
-//     vid.api("pause");
-// }
 
-
+// PAUSE REEL WHEN CLOSED
+const videoReel = document.getElementById('video');
+// saves the current iframe source
+const vidsrc = videoReel.getAttribute('src');
+function pauseReel() {
+    // sets the source to nothing, stopping the video
+    videoReel.setAttribute('src',''); 
+    // sets it back to the correct link so that it reloads immediately on the next window open
+    videoReel.setAttribute('src', vidsrc);
+}
 
 // HAMBURGER MENU
 const burgerBtn = document.querySelector('.burger-btn');
@@ -41,39 +41,3 @@ closeBurger.addEventListener('click', () => {
 function closeMobileNav() {
     mobileNav.classList.remove('show-mobile-nav');
 }
-
-// PAUSE REEL WHEN CLOSED
-const videoReel = document.getElementById('video');
-// saves the current iframe source
-const vidsrc = videoReel.getAttribute('src');
-function pauseReel() {
-    // sets the source to nothing, stopping the video
-    videoReel.setAttribute('src',''); 
-    // sets it back to the correct link so that it reloads immediately on the next window open
-    videoReel.setAttribute('src', vidsrc);
-}
-
-
-// // SHOW MORE VIDEOS ON MOBILE
-// const moreVideos = document.querySelector('.more-videos');
-// moreVideos.addEventListener('click', () => {
-    
-// })
-function videosToggle() {
-    const moreVideos = document.querySelector(".more-videos");
-    const lessVideos = document.querySelector(".less-videos");
-    const hideableVideos = document.querySelectorAll(".hideable");
-    if (moreVideos.style.display === "none") {
-      moreVideos.style.display = "block";
-      lessVideos.style.display = "none";
-      for(var i=0; i< hideableVideos.length; i++){
-        hideableVideos[i].style.display = "none";
-    }
-    } else {
-      moreVideos.style.display = "none";
-      lessVideos.style.display = "block";
-      for(var i=0; i< hideableVideos.length; i++){
-        hideableVideos[i].style.display = "block";
-    }
-    }
-  } 
